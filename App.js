c0b111b0bbc670567d301d4621c719a747129488
marizona <marie.jacquier@epitech.eu>
2021-01-20 17:21:05 +0100

    login imgur

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Linking } from 'react-native';
 
-export default function App() {
+
+Linking.getInitialURL().then(url =>{
+console.log(url);
+})
+Linking.addEventListener("/oauth/callback", getData)
+function getData(e){
+  console.log(e);
+}
+
+async function goApp(){
+  await Linking.openURL('https://api.imgur.com/oauth2/authorize?client_id=04f7ce5c8c9437f&response_type=token');
+}
+
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>
+        hello world!
+      </Text>
+      <Button
+      onPress={goApp}
+      title="go to app"
+      color="blue"
+      accessibilityLabel="go to app"/>
+<StatusBar style="auto"/>
     </View>
   );
 }

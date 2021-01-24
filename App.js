cd9a7079c00033c9c93b2c6e1287a873b12cc1cb
marizona <marie.jacquier@epitech.eu>
2021-01-24 11:54:29 +0100

    profile

--- App.js
+++ App.js
@@ -72,6 +72,7 @@ export default function App() {
             showUpload={showUpload} /> 
             
            <User /> */}
+           <Profile/>
         </SafeAreaView>
 
       )}
--- Profile.js
+++ Profile.js
@@ -1,99 +1,114 @@
 import React, { useState } from 'react';
-import { Image, View, SafeAreaView, CardList, ScrollView, StyleSheet,Text } from 'react-native';
+import { Image, View, SafeAreaView, FlatList, ScrollView, StyleSheet, Text } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 
 const Profile = () => {
 
-    const [username, setUsername] = useState(null);
-    const [imagePath, setImagePath] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [imagePath, setImagePath] = useState(null);
+  const [UserData, setUserData] = useState(null);
+
+  // setUserData([])
+  const imagedata = [{ key: 0, image: { uri: "https://bootdey.com/img/Content/avatar/avatar6.png" } },
+  { key: 1, image: { uri: "https://bootdey.com/img/Content/avatar/avatar6.png" } },
+  { key: 2, image: { uri: "https://bootdey.com/img/Content/avatar/avatar6.png" } }];
+
+  const RenderImage = ({ item }) => {
+    return (<View style={{ flex: 1 }} key={item}>
+      <Image style={styles.profileImage} source={item.image} />
+    </View>)
+  }
 
 
-    const getUserData = async () => {
-        const jsonValue = await AsyncStorage.getItem('user_data');
-        const data = JSON.parse(jsonValue)
-        console.log(data.username)
-        let clientID = 'adcadfe92139167';
-        let token = data.access_token;
+  const getUserData = async () => {
+    const jsonValue = await AsyncStorage.getItem("user_data");
+    const data = JSON.parse(jsonValue);
+    console.log(data.username);
+    let clientID = "adcadfe92139167";
+    let token = data.acess_token;
 
-        setUsername(data.username)
-        axios
-            .get(
-                `https://api.imgur.com/3/account/me/images`,
-                {
-                    Authorization: {
+    console.log(token)
+    console.log(clientID)
+    setUsername(data.username);
+    axios
+      .get('https://api.imgur.com/3/account/me/images', {
 
-                        'Token': `${token}`,
-                    },
-                    headers: {
-                        'Authorization': `Client-ID ${clientID}`,
-                    },
-                }
-            )
-            .then(
-                (response) => {
-                    console.log(response.data.data.link);
-                    setImagePath(response.data.data.link)
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        // console.log(response.data);
+        // setUserData(response.data)
+        // console.log(response.data.data);
+        // console.log('test')
+        const img = response.data.data.map(item => item.link)
+        console.log(img)
+        // UserData.map((userData) => {
+        //  console.log(userData.link);
+        //         });
+        // UserData.forEach(element => {
+        //   console.log(element.link)
+        // });
 
-                })
-            .catch(
-                (error) => {
-                    console.log(error);
-                });
+        setImagePath(response.data.link);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  getUserData();
 
-    }
-    getUserData()
+  return (
 
-    return (
-        
-        <ScrollView style={styles.scrollContainer}>
-        <View style={styles.container}>
-          <View style={styles.box}>
-            <Image style={styles.profileImage} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
-            <Text style={styles.name}>{username}</Text>
-          </View>
-          </View>
-      </ScrollView>
-    );
-    
+    <ScrollView style={styles.scrollContainer}>
+
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Image style={styles.profileImage} source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
+          <Text style={styles.name}>{username}</Text>
+        </View>
+        <FlatList showsHorizontalScrollIndicator={false} data={imagedata} renderItem={RenderImage} keyExtractor={item => item.id} />
+      </View>
+    </ScrollView>
+  );
+
 
 };
 
 const styles = StyleSheet.create({
-    scrollContainer:{
-      flex: 1,
+  scrollContainer: {
+    flex: 1,
+  },
+  container: {
+    padding: 20,
+  },
+  box: {
+    marginTop: 10,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOpacity: .2,
+    shadowOffset: {
+      height: 1,
+      width: -2
     },
-    container:{
-      padding:20,
-    },
-    box: {
-      marginTop:10,
-      backgroundColor: 'white',
-      alignItems: 'center',
-      shadowColor: 'black',
-      shadowOpacity: .2,
-      shadowOffset: {
-        height:1,
-        width:-2
-      },
-      elevation:2,
-      paddingTop:10
-    },
-    profileImage:{
-      width:100,
-      height:100,
-      marginBottom:20,
-      borderRadius:300,
-    },
-    name:{
-      fontSize:25,
-      marginBottom:20,
-      color: '#1E90FF',
-    },
-    
-    
+    elevation: 2,
+    paddingTop: 10
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 300,
+  },
+  name: {
+    fontSize: 25,
+    marginBottom: 20,
+    color: '#1E90FF',
+  },
+
+
 })
-
-
-
 export default Profile;
